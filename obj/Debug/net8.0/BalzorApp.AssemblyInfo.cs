56fe040ef82929180c602d17b2c832909bdbe535
thir2608 <robert.mbassine.thiare@usherbrooke.ca>
2024-05-23 15:36:15 -0400

    Linker ma feuille de style

--- obj/Debug/net8.0/BalzorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BalzorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BalzorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42a4ff8b98169b58e87c69eeb4bc08f41220c2cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53d35a9c0ab2019e4bc32cc019221eb3925985c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BalzorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BalzorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
