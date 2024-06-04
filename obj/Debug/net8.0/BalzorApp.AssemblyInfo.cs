58ccfa785a82eafda00a8c121b0bd43abb54c630
thiarerobert <thiarerobert07@protonmail.com>
2024-06-04 15:20:23 -0400

    Ajout de EntityFrameworksqlite

--- obj/Debug/net8.0/BalzorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BalzorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BalzorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56fe040ef82929180c602d17b2c832909bdbe535")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a70b2fc5c9175bb7769d4800a57bb74886d22838")]
 [assembly: System.Reflection.AssemblyProductAttribute("BalzorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BalzorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
