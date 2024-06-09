198d5d81bdd85ac64d6b917b04332d139b1b4459
thiarerobert <thiarerobert07@protonmail.com>
2024-06-08 22:08:10 -0400

    Initialisation des données dans SQLite

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 using BalzorApp.Components;
+using BlazorLearning.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpClient();
+builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddServerSideBlazor();
@@ -21,5 +24,16 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+// Initialize the database
+var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+using (var scope = scopeFactory.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (db.Database.EnsureCreated())
+    {
+        SeedData.Initialize(db);
+    }
+}
 
 app.Run();
--- obj/Debug/net8.0/BalzorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BalzorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BalzorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a70b2fc5c9175bb7769d4800a57bb74886d22838")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58ccfa785a82eafda00a8c121b0bd43abb54c630")]
 [assembly: System.Reflection.AssemblyProductAttribute("BalzorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BalzorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
